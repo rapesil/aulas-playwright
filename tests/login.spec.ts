d81ceb8f83692d478d06f63d003a43c9c174d1b0
Rafael <rapesil15@gmail.com>
2024-10-07 20:08:21 -0300

    Cria testes para tela de login

---
+++ tests/login.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test('Deve fazer login com sucesso', async({ page }) => {
+            await page.goto('https://the-internet.herokuapp.com/login');
+            await page.locator('#username').fill('tomsmith');
+            await page.locator('#password').fill('SuperSecretPassword!');
+            await page.locator('.fa-sign-in').click();
+
+            const successMessage = page.locator('.success');
+            expect(successMessage).toBeVisible();
+            expect(await successMessage.textContent()).toContain('You logged into a secure area!')
+            expect(page.url()).toBe('https://the-internet.herokuapp.com/secure')
+});
+
+test('Deve impedir acesso caso senha esteja incorreta', async({ page })=> {
+            await page.goto('https://the-internet.herokuapp.com/login');
+            await page.locator('#username').fill('tomsmith');
+            await page.locator('#password').fill('senha-invalida');
+            await page.locator('.fa-sign-in').click();
+
+            const errorMessage = page.locator('.error');
+            expect(errorMessage).toBeVisible();
+            expect(await errorMessage.textContent()).toContain('Your password is invalid!')
+            expect(page.url()).toBe('https://the-internet.herokuapp.com/login')
+});
+
+test('Deve impedir acesso caso usuário não seja válido', async({ page }) => {
+            await page.goto('https://the-internet.herokuapp.com/login');
+            await page.locator('#username').fill('fakeUser');
+            await page.locator('#password').fill('SuperSecretPassword!');
+            await page.locator('.fa-sign-in').click();
+
+            const errorMessage = page.locator('.error');
+            expect(errorMessage).toBeVisible();
+            expect(await errorMessage.textContent()).toContain('Your username is invalid!')
+            expect(page.url()).toBe('https://the-internet.herokuapp.com/login')
+});
--- tests/primeiro-test.spec.ts
+++ tests/primeiro-test.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('Deve acessar a página inicial do Google', async ({ page }) => {
+test('Deve acessar a página inicial do Google', async ({ page }) => {
             await page.goto('https://www.google.com');  
 
             await expect(page).toHaveTitle('Google');
